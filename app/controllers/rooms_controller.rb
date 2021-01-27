e3105bd553954e00afb29ccea044a6586ecef196
K-OSG <gongonta6886@gmail.com>
2021-01-27 22:08:05 +0900

    チャットルームの削除機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -15,6 +15,14 @@ class RoomsController < ApplicationController
     end
   end
 
+  def destroy
+    # Roomモデルから個別指定データを取り出す記述
+    # モデル→コントローラー、コントローラー→モデルのデータの受け渡しはparams使用
+    room = Room.find(params[:id])
+    room.destroy
+    redirect_to root_path
+  end
+
   private
   def room_params
     params.require(:room).permit(:name, user_ids: [])
